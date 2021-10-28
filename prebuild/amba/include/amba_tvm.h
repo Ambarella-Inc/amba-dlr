ddfd8837964f336cbe8fc6df510c62f1f3b25401
cychenc <cychenc@ambarella.com>
2021-10-28 15:50:35 +0800

    Revert "fix the strucutre size to make it 64-byte alignment"
    This reverts commit b0d3cf72c6004b94ce35a183aca37ba6152cdf6b.

--- prebuild/amba/include/amba_tvm.h
+++ prebuild/amba/include/amba_tvm.h
@@ -50,8 +50,8 @@ extern "C" {
 typedef struct {
 	IN const char* engine_name;
 	IN const char* engine_filepath;
-	INOUT unsigned long engine_id;
-	uint32_t reserve[10];
+	INOUT uint32_t engine_id;
+	uint32_t reserve[11];
 } amba_engine_cfg_t;
 
 typedef struct {
